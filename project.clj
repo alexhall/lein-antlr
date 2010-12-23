f098d201c21e1781289a692e7db5bdf4f5cd36ba
alexhall <alexhall@revelytix.com>
2010-12-23 17:48:08 -0500

    Adding dev-dependencies to allow the tests to run

--- project.clj
+++ project.clj
@@ -1,7 +1,11 @@
 (defproject lein-antlr "0.1.0-SNAPSHOT"
   :description "Generate source code from ANTLR grammars in Leiningen."
   :dependencies [[org.antlr/antlr "3.2"]]
-  :eval-in-leiningen true
+  
+  :dev-dependencies [[org.clojure/clojure "1.2.0"]
+                     [org.clojure/clojure-contrib "1.2.0"]
+                     [org.antlr/antlr "3.2"]
+                     [leiningen "1.3.1"]]
   
   :url "http://github.com/alexhall/lein-antlr"
 )
