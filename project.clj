7d12733ef096f2c77ebf02c6d1b2edb7f40ebba4
alexhall <alexhall@revelytix.com>
2010-12-23 15:25:09 -0500

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject lein-antlr "0.1.0-SNAPSHOT"
+  :description "Generate source code from ANTLR grammars in Leiningen."
+  :dependencies [[org.antlr/antlr "3.2"]]
+  :eval-in-leiningen true
+  
+  :url "http://github.com/alexhall/lein-antlr"
+)
