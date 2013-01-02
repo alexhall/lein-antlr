954c91ea59f419b43b39c1060fc1149f5897aea6
alexhall <alexhall@revelytix.com>
2013-01-02 12:14:38 -0500

    Add license info to project.clj

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :dependencies [[org.antlr/antlr "3.4"]]
   :profile {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}}
   :url "http://github.com/alexhall/lein-antlr"
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+  :license {:name "Apache Software License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"
+			:distribution :repo})
