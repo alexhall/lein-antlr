bd15133c54427738d7a6cc8cf85392f00fd01238
alexhall <alexhall@revelytix.com>
2013-01-02 12:40:02 -0500

    bump version to 0.2.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-antlr "0.2.0"
+(defproject lein-antlr "0.2.1-SNAPSHOT"
   :description "Generate source code from ANTLR grammars in Leiningen."
   :dependencies [[org.antlr/antlr "3.4"]]
   :profile {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}}
