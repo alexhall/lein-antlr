b3332aa9cb0eb08c3f4dbd9be8ec4374f05c0f01
alexhall <alexhall@revelytix.com>
2013-01-02 11:44:35 -0500

    Add :eval-in-leiningen for development

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Generate source code from ANTLR grammars in Leiningen."
   :dependencies [[org.antlr/antlr "3.4"]]
   :profile {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}}
-  :url "http://github.com/alexhall/lein-antlr")
+  :url "http://github.com/alexhall/lein-antlr"
+  :eval-in-leiningen true)
