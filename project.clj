15a1abc722e6f256c04cd11941e05e13f66e5404
Alex Hall <alex.hall@teradata.com>
2015-02-17 10:29:57 -0500

    Upgrade to latest ANTLR 4.5

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject lein-antlr "0.2.2-SNAPSHOT"
   :description "Generate source code from ANTLR grammars in Leiningen."
-  :dependencies [[org.antlr/antlr4 "4.0"]]
+  :dependencies [[org.antlr/antlr4 "4.5"]]
   :profile {:dev {:dependencies [[org.clojure/clojure "1.4.0"]]}}
   :url "http://github.com/alexhall/lein-antlr"
   :eval-in-leiningen true
